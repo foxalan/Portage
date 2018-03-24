2e8077fc0729a908da8c3d7e3d2f5ed5ced05265
foxalan <33643467+foxalan@users.noreply.github.com>
2018-03-24 11:46:14 +0800

    add hello map

---
+++ app/src/main/java/com/example/administrator/protage/baidumap/BaiduMapActivty.java
@@ -0,0 +1,19 @@
+package com.example.administrator.protage.baidumap;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.administrator.protage.base.BaseActivity;
+
+/**
+ * @author  alan
+ * 百度地图的使用.
+ */
+
+public class BaiduMapActivty extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/protage/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.administrator.protage.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/3/24 0024.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
