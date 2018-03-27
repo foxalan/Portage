e691578dcd7992f1a6d6313c293c110ae0842755
foxalan <33643467+foxalan@users.noreply.github.com>
2018-03-27 09:22:07 +0800

    add ui library

---
+++ app/src/main/java/com/example/administrator/protage/application/FoxApp.java
@@ -0,0 +1,18 @@
+package com.example.administrator.protage.application;
+
+import android.app.Application;
+
+/**
+ * @Author Alan
+ * Date 2018/3/27 0027
+ * Function 用于初始化应用启动时的一些数据
+ * Issue
+ */
+
+public class FoxApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
