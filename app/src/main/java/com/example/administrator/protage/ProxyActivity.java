51b86193db310398681fe2f87e4c10f5c764226d
foxalan <33643467+foxalan@users.noreply.github.com>
2018-05-04 11:06:57 +0800

    finish activity

--- app/src/main/java/com/example/administrator/protage/ProxyActivity.java
+++ app/src/main/java/com/example/administrator/protage/ProxyActivity.java
@@ -2,11 +2,15 @@ package com.example.administrator.protage;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ContentFrameLayout;
 
 import com.example.fox_core.fragment.LatteDelegate;
 
-import me.yokeyword.fragmentation.SupportActivity;
+import me.yokeyword.fragmentation.ExtraTransaction;
+import me.yokeyword.fragmentation.ISupportActivity;
+import me.yokeyword.fragmentation.SupportActivityDelegate;
+import me.yokeyword.fragmentation.anim.FragmentAnimator;
 
 /**
  * @Author Alan
@@ -15,13 +19,20 @@ import me.yokeyword.fragmentation.SupportActivity;
  * Issue
  */
 
-public abstract class ProxyActivity extends SupportActivity {
+public abstract class ProxyActivity  extends AppCompatActivity implements ISupportActivity {
 
-    public  abstract LatteDelegate setRootDelegate();
+    /**
+     * 设置第一个Delegate
+     * @return
+     */
+    public abstract LatteDelegate setRootDelegate();
+
+    private final SupportActivityDelegate DELEGATE = new SupportActivityDelegate(this);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DELEGATE.onCreate(savedInstanceState);
         initContainer(savedInstanceState);
     }
 
@@ -30,14 +41,50 @@ public abstract class ProxyActivity extends SupportActivity {
         contentFrameLayout.setId(R.id.delegate_container);
         setContentView(contentFrameLayout);
         if (savedInstanceState == null) {
-            loadRootFragment(R.id.delegate_container,setRootDelegate());
+            DELEGATE.loadRootFragment(R.id.delegate_container,setRootDelegate());
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        DELEGATE.onDestroy();
         System.gc();
         System.runFinalization();
+    }
+
+    @Override
+    public SupportActivityDelegate getSupportDelegate() {
+        return DELEGATE;
+    }
+
+    @Override
+    public ExtraTransaction extraTransaction() {
+        return DELEGATE.extraTransaction();
+    }
+
+    @Override
+    public FragmentAnimator getFragmentAnimator() {
+        return DELEGATE.getFragmentAnimator();
+    }
+
+    @Override
+    public void setFragmentAnimator(FragmentAnimator fragmentAnimator) {
+        DELEGATE.setFragmentAnimator(fragmentAnimator);
+    }
+
+    @Override
+    public FragmentAnimator onCreateFragmentAnimator() {
+        return DELEGATE.getFragmentAnimator();
+    }
+
+    @Override
+    public void onBackPressedSupport() {
+        DELEGATE.onBackPressedSupport();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DELEGATE.onBackPressed();
     }
 }
